026ebb1c2f0a52025e45fa19661efe2504ffc6a7
eto1302 <martinangelov1302@gmail.com>
2020-04-08 16:19:31 +0300

    Modified Order Listing and added Order Details Page

--- src/components/Orders/All.vue
+++ src/components/Orders/All.vue
@@ -1,22 +1,37 @@
 <template>
   <div class="jumbotron mx-auto bg-color">
     <h1 class="text-center">All Orders</h1>
-    <hr class="eventures-bg-color hr-2 w-75" />
+    <hr class="hr-2 w-75" />
     <table class="table w-75 mx-auto table-hover border-top-0">
       <thead>
         <tr>
           <th scope="col">RecordName</th>
           <th scope="col">RecordArtist</th>
-          <th scope="col">User Email</th>
           <th scope="col">Cost</th>
+          <th scope="col">Actions</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="order in orders" :key="order.recordName">
           <th scope="row">{{order.recordName}}</th>
           <td>{{order.recordArtist}}</td>
-          <td>{{order.userEmail}}</td>
           <td>${{order.cost}}</td>
+          <td>
+            <router-link
+              tag="button"
+              :to="'/orders/details/' + order.id"
+              href
+              type="button"
+              class="btn btn-success col-3"
+            >Details</router-link>
+
+            <button
+              tag="button"
+              @click="removeOrder(order.id)"
+              type="button"
+              class="btn btn-danger col-3"
+            >Delete</button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -26,13 +41,14 @@
 <script>
 import { mapState } from "vuex";
 export default {
+  methods: {
+    removeOrder: function(id) {
+    this.$store.commit("removeOrder", id);
+    this.$router.push("/orders/all");
+    }
+  },
   computed: {
     ...mapState(["orders"])
-  },
-  method: {
-    deleteOrder() {
-      console.log("sfafasffaasffasafs");
-    }
   }
 };
 </script>
---
+++ src/components/Orders/Details.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <hr class="hr-2 bg-dark" />
+    <h3 class="text-center">Name of the Record: {{selectedOrder.recordName}}</h3>
+    <h3 class="text-center">Name of the Artist: {{selectedOrder.recordArtist}}</h3>    
+    <h3 class="text-center">Genre: {{selectedOrder.recordGenre}}</h3>
+    <hr class="hr-2 bg-dark" />
+    <h3 class="text-center">Email of the User: {{selectedOrder.userEmail}}</h3>
+    <h3 class="text-center">Date Created: {{selectedOrder.dateCreated}}</h3>
+
+    <img :src="selectedOrder.recordImgUrl" class="text-center" style="width:488px; height:488;" />
+    <hr class="hr-2 bg-dark" />
+    <h3 class="text-center">Cost: ${{selectedOrder.cost}}</h3>
+    <hr class="hr-2 bg-dark" />
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default { 
+  created: function() {
+    this.$store.commit("selectOrder", this.$route.params.id);
+    this.$store.dispatch("fetchUserProfile");
+  },
+  computed: {
+    ...mapState(["selectedOrder"]),
+    ...mapState(["currentUser"])
+  }
+};
+</script>
+
+<style>
+h2 {
+  color: blue;
+}
+</style>
